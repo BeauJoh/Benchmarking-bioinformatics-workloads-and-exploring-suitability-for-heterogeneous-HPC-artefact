6567e1af0f17e6c2d3b2af7f262e076a913e3076
Beau Johnston <beau@inbeta.org>
2018-12-07 18:45:15 +1100

    updated final swat figure -- finally

--- analysis_tools/bar_charts.R
+++ analysis_tools/bar_charts.R
@@ -257,45 +257,48 @@ dev_name<- c('E5-2697','i7-6700K','Titan X','GTX 1080','GTX 1080 Ti',"K20m","K40
 dev_type<- c('CPU','CPU','Consumer GPU','Consumer GPU','Consumer GPU','HPC GPU','HPC GPU','HPC GPU','Consumer GPU','Consumer GPU','MIC')
 sizes <- c('small')
 data.swat <- data.frame()
-columns <- c('region','id','time','overhead')
+columns <- c('region','repeats_to_two_seconds','id','time','overhead')
 
 index <- 1
 for(device in devices){
-        for(size in sizes){
-                    path = paste("/workspace/data/time_data/",device,"_swat_",size,"_time.0/",sep='')
+    for(size in sizes){
+        path = paste("/workspace/data/time_data/",device,"_swat_",size,"_time.0/",sep='')
         print(paste("loading:",path))
-                x <- ReadAllFilesInDir.AggregateWithRunIndex(dir.path=path,col=columns)
-                x$device <- device
-                        x$size <- size
-                        x$dev_name <- dev_name[index]
-                                x$dev_type <- dev_type[index]
-                                data.swat <- rbind(data.swat, x)
-                                    }
+        x <- ReadAllFilesInDir.AggregateWithRunIndex(dir.path=path,col=columns)
+        x$device <- device
+        x$size <- size
+        x$dev_name <- dev_name[index]
+        x$dev_type <- dev_type[index]
+        data.swat <- rbind(data.swat, x)
+    }
     index <- index + 1
 }
 
 SumPerRunReduction <- function(x){
-        z <- data.frame()
+    z <- data.frame()
     for (y in unique(x$run)){
-                z <- rbind(z,data.frame('time'=sum(x[x$run == y,]$time),'run'=y))
-        }
-        return(z)
+        m <- x[x$run == y,]
+        #averaged by the number of iterations used to fill the 2 second interval
+        #print(paste('device = ',unique(x$device),'time (us) = ',sum(m$time)/(max(m$repeats_to_two_seconds)+1)))
+        z <- rbind(z,data.frame('time'=sum(m$time)/(max(m$repeats_to_two_seconds)+1),'run'=y))
+    }
+    return(z)
 }
 
 data.procswat <- data.frame()
 
 for(device in devices){
-        for(size in sizes){
-                    x <- data.swat[(data.swat$region=="hSetZeroKernel_kernel" | data.swat$region=="hTraceBackKernel" | data.swat$region=="hMatchStringKernel_kernel") & data.swat$device == device & data.swat$size == size,]
+    for(size in sizes){
+        x <- data.swat[(data.swat$region=="hTraceBackKernel" | data.swat$region=="hMatchStringKernel_kernel") & data.swat$device == device & data.swat$size == size,]
         y <- x
-                x <- SumPerRunReduction(x)
-                data.procswat <- rbind(data.procswat,data.frame('application'='sw',
-                                                                                                                        'device'=y$dev_name,
-                                                                                                                                                                                'accelerator_type'=y$dev_type,
-                                                                                                                                                                                'size'=size,
-                                                                                                                                                                                                                                        'time'=x$time,
-                                                                                                                                                                                                                                        'run'=x$run))
-                    }
+        x <- SumPerRunReduction(x)
+        data.procswat <- rbind(data.procswat,data.frame('application'='sw',
+                                                        'device'=unique(y$dev_name),
+                                                        'accelerator_type'=unique(y$dev_type),
+                                                        'size'=size,
+                                                        'time'=x$time,
+                                                        'run'=x$run))
+    }
 }
 
 #remove knl from results
